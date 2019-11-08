464622af035ce6d0046551e1a785dfb0fd0e8bb3
Melinda Santana <melinda.santana@gmail.com>
2019-11-07 19:39:29 -0800

    creating shell of the idea
    creating shell of the idea

--- mentalfloat/src/App.js
+++ mentalfloat/src/App.js
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>How are you looking to feel better today?</h1>
+        <h2></h2>
+        <button>Sleep Better</button>
+        <button>Take Better Care of Yourself</button>
+        <button>Take a moment to meditate</button>
+        <button>Read something Motivational</button>
+        <button>Find someone to talk to</button>
+        <button>Locate Recreational Support</button>
       </header>
     </div>
   );
