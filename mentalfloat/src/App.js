18aacc27d1b0a7e69522845b829fb6c7afb46762
Melinda Santana <melinda.santana@gmail.com>
2019-11-16 11:30:27 -0800

    updating layout and photo routing
    updating layout and photo routing

--- mentalfloat/src/App.js
+++ mentalfloat/src/App.js
@@ -6,16 +6,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/meditate.jpeg" className="App-logo" alt="logo" />
+        <img src="/mindfloat.jpeg" className="App-logo" alt="logo" />
+        </header>
         <h1>How are you looking to feel better today?</h1>
         <h2></h2>
-        <button>Sleep Better</button>
-        <button>Take Better Care of Yourself</button>
-        <Button text='Take a moment to meditate' src="/meditate.jpeg" href="https://www.google.com"/>
-        <button>Read something Motivational</button>
-        <button>Find someone to talk to</button>
-        <button>Locate Recreational Support</button>
-      </header>
+        <div class="row">
+        <div class="column">
+        <Button text='Alternative Sleep Support' src="/sleep.jpeg" href="https://www.google.com"/>
+        <Button text='Take a Moment to Meditate' src="/meditate.jpeg" href="https://www.google.com"/>
+        </div>
+        <div class="column">
+        <Button text='Read Something Motivational' src="/motivation.jpeg" href="https://www.google.com"/>
+        <Button text='Find Someone to Talk To' src="/therapy.jpeg" href="https://www.google.com"/>
+        </div>
+        <div class="column">
+        <Button text='Get Moving' src="/workout.jpeg" href="https://www.google.com"/>
+        <Button text='Get Outside' src="/outside.jpeg" href="https://www.google.com"/>
+        </div>
+        </div>
+
     </div>
   );
 }
