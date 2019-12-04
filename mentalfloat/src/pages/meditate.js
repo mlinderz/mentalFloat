01030f101d9665912104f02f77441866b46742a8
Melinda Santana <melinda.santana@gmail.com>
2019-12-03 20:38:11 -0800

    fixing meditation formatting

--- mentalfloat/src/pages/meditate.js
+++ mentalfloat/src/pages/meditate.js
@@ -9,7 +9,7 @@ function MeditatePage() {
       <img src="/mentalfloat.png" className="App-logo" alt="logo" />
         </header>
         <h1>Guided Meditatation</h1>
-        <h3> <ReactPlayer url='https://www.youtube.com/watch?v=4EaMJOo1jks' playing /></h3>
+        <h3> <div className='player-wrapper'><ReactPlayer url='https://www.youtube.com/watch?v=4EaMJOo1jks' playing /></div></h3>
     </div>
   );
 }
